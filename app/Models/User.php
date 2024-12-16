ba73d4df081e3a97474c752f32247312488a7d7f
Paul Schur <78649242+pschur@users.noreply.github.com>
2024-12-16 17:29:40 +0100

    install devdojo auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,10 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Devdojo\Auth\Models\User as AuthUser;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends AuthUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
