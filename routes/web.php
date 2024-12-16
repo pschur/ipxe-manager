8f74170d3dfd035e302e863b837d32fe78cbb8c7
Paul Schur <78649242+pschur@users.noreply.github.com>
2024-12-16 17:31:09 +0100

    add new theme and main page

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/', function () {
+        return view('index');
+    });
 });
